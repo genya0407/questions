7b8426b9b24c312f9526c95799cda29d81ecbd01
Yusuke Sangenya <longinus.eva@gmail.com>
2017-08-15 16:33:39 +0900

    interface & default implementation.

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,8 @@
 use std::ops::Deref;
 use r2d2_postgres::PostgresConnectionManager;
 use r2d2_postgres::postgres::Connection;
-use r2d2_postgres::postgres::types::ToSql;
+use r2d2_postgres::postgres::rows::Row;
+use r2d2_postgres::postgres::types::{ToSql, FromSql};
 use r2d2;
 
 use std::collections::HashMap;
@@ -17,11 +18,39 @@ impl Deref for DbConn {
     }
 }
 
-trait ActiveRecord : Sized {
-	fn new(params: HashMap<&str, &ToSql>) -> Self;
-	fn create(params: HashMap<&str, &ToSql>) -> Self;
-	fn find(id: i64) -> Self;
-	fn hoge(condition: String) -> Vec<Self>;
-	fn save(&self) -> bool;
-	fn delete(&self) -> bool;
+trait ActiveRecord<'a> : Sized {
+	fn table_name() -> &'static str;
+	fn from_row(conn: &DbConn, row: Row) -> Self;
+	fn conn(&self) -> &DbConn;
+
+	fn columns(&self) -> HashMap<&str, &str>;
+	fn find(conn: &DbConn, id: i64) -> Self {
+		let sql = format!("SELECT * FROM {} WHERE id = ? LIMIT 1", Self::table_name());
+		let rows = conn.query(&sql, &[&id]).unwrap();
+		Self::from_row(conn, rows.get(0))
+	}
+	fn lit(conn: &DbConn, condition: &str) -> Vec<Self> {
+		let sql = format!("SELECT * FROM {} WHERE {}", Self::table_name(), condition);
+		let mut results = Vec::new();
+		for row in conn.query(&sql, &[]).unwrap().into_iter() {
+			results.push(Self::from_row(conn, row));
+		}
+		results
+	}
+	fn update(&self) {
+		let set_stmt = self.columns().into_iter()
+                             .map(|(k, v)| format!("{} = {}", k, v))
+                             .collect::<Vec<String>>()
+                             .join(", ");
+		let stmt = format!(
+			"UPDATE {} SET {} WHERE id = {}",
+			Self::table_name(), set_stmt, self.primary_key().unwrap().to_string()
+		);
+		self.conn().execute(&stmt, &[]).unwrap();
+	}
+	fn delete(&self) {
+		let stmt = format!("DELETE FROM {} WHERE id = {}", Self::table_name(), self.primary_key().unwrap().to_string());
+		self.conn().execute(&stmt, &[]).unwrap();
+	}
+	fn primary_key(&self) -> Option<i64>;
 }
